837470af9be4aeccad4fbb165cf0b9895b86eedf
AlexandreVianaDev <alexandreviana94@hotmail.com>
2022-12-01 17:57:55 -0300

    HTML e CSS concluídos

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+// function showModal () {
+
+// }
+
+// function prepareModal () {
+//     const modalOpenButtons = document.querySelectorAll("[data-control-modal]");
+
+//     for (let i = 0; i < modalOpenButtons.length; i++) {
+//         const modal = document.querySelector
+
+//         let modalButton = modalOpenButtons[i];
+//         modalButton.addEventListener("click", ()=>{
+
+//         })
+//     }
+// }
